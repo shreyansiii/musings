0d03929b658dd9c0b41c527386f5be844db273ad
shreyansishrestha <shreyansishrestha@uxqode.co>
2026-07-24 14:59:33 +0545

    add keyword search filter to content pieces API

--- content/views.py
+++ content/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Genre, ContentPiece
 from .serializers import GenreSerializer, ContentPieceListSerializer, ContentPieceDetailSerializer
 
@@ -12,6 +12,8 @@ class GenreViewSet(viewsets.ReadOnlyModelViewSet):
 class ContentPieceViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ContentPiece.objects.filter(status="published")
     lookup_field = "slug"
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["title", "subtitle", "body"]
 
     def get_serializer_class(self):
         if self.action == "retrieve":
@@ -24,3 +26,33 @@ class ContentPieceViewSet(viewsets.ReadOnlyModelViewSet):
         if genre_slug:
             qs = qs.filter(genre__slug=genre_slug)
         return qs
+
+
+
+
+# from rest_framework import viewsets
+# from .models import Genre, ContentPiece
+# from .serializers import GenreSerializer, ContentPieceListSerializer, ContentPieceDetailSerializer
+
+
+# class GenreViewSet(viewsets.ReadOnlyModelViewSet):
+#     queryset = Genre.objects.all()
+#     serializer_class = GenreSerializer
+#     lookup_field = "slug"
+
+
+# class ContentPieceViewSet(viewsets.ReadOnlyModelViewSet):
+#     queryset = ContentPiece.objects.filter(status="published")
+#     lookup_field = "slug"
+
+#     def get_serializer_class(self):
+#         if self.action == "retrieve":
+#             return ContentPieceDetailSerializer
+#         return ContentPieceListSerializer
+
+#     def get_queryset(self):
+#         qs = super().get_queryset()
+#         genre_slug = self.request.query_params.get("genre")
+#         if genre_slug:
+#             qs = qs.filter(genre__slug=genre_slug)
+#         return qs
