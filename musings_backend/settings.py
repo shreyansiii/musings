3a2cefa27ac0c623f52558d11ceffb3fa93cfded
shreyansishrestha <shreyansishrestha@uxqode.co>
2026-08-03 15:43:00 +0545

    added loggin in settings.py for render

--- musings_backend/settings.py
+++ musings_backend/settings.py
@@ -182,3 +182,25 @@ TINYMCE_DEFAULT_CONFIG = {
         "removeformat | code fullscreen"
     ),
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
